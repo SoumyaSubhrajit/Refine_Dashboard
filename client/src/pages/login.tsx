69d6d633aeca26766e9bacc563093942a6734c1a
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-07-01 13:06:45 +0530

    Adding some element to the codebase.

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography';
 import { ThemedTitleV2 } from '@refinedev/mui';
 
 import { CredentialResponse } from '../interfaces/google';
+import { yariga } from '../assets';
 
 // Todo: Update your Google Client ID here
 const GOOGLE_CLIENT_ID =
@@ -26,7 +27,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: 'popup',
-          client_id: GOOGLE_CLIENT_ID,
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -47,40 +48,35 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <Container
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Box
-        display='flex'
-        gap='36px'
-        justifyContent='center'
-        flexDirection='column'
+    <Box component='div' sx={{ backgroundColor: '#FCFCFC' }}>
+      <Container
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
-        <ThemedTitleV2
+        <Box
+          display='flex'
+          gap='36px'
+          justifyContent='center'
+          flexDirection='column'
+          sx={{ backgroundColor: '#FCFCFC' }}
+        >
+          {/* <ThemedTitleV2
           collapsed={false}
           wrapperStyles={{
             fontSize: '22px',
             justifyContent: 'center',
           }}
-        />
-
-        <GoogleButton />
-
-        <Typography align='center' color={'text.secondary'} fontSize='12px'>
-          Powered by
-          <img
-            style={{ padding: '0 5px' }}
-            alt='Google'
-            src='https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg'
-          />
-          Google
-        </Typography>
-      </Box>
-    </Container>
+        /> */}
+          <div>
+            <img src={yariga} alt='Yariga Logo' />
+          </div>
+          <GoogleButton />{' '}
+        </Box>
+      </Container>
+    </Box>
   );
 };
